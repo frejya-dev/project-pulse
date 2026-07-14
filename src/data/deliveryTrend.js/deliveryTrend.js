f253da593f65d560c12b007bdb0ccfdc6a115ce3
Frejya Lindh <frejya.lindh.hernandez@gmail.com>
2026-07-14 00:16:32 +0000

    Add chart data

---
+++ src/data/deliveryTrend.js/deliveryTrend.js
@@ -0,0 +1,8 @@
+export const deliveryTrend = [
+  { month: "Jan", completion: 72 },
+  { month: "Feb", completion: 75 },
+  { month: "Mar", completion: 79 },
+  { month: "Apr", completion: 84 },
+  { month: "May", completion: 81 },
+  { month: "Jun", completion: 87 },
+];
